23b0f38246b210136d326e3aa40ae53674b4c3f1
Breno Batista Machado <brenomachado@gmail.com>
2023-01-10 21:53:43 -0800

    Add Two Numbers

---
+++ LeetCodeChallengers/LinkedList/AddTwoNumbersSolution02.cs
@@ -0,0 +1,85 @@
+﻿namespace LeetCodeChallengers.LinkedList;
+
+/*
+ * You are given two non-empty linked lists representing two non-negative integers. 
+ * The digits are stored in reverse order, and each of their nodes contains a single digit. 
+ * Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * 
+ * Example 1:
+ * 
+ * 2 -> 4 -> 3
+ * 5 -> 6 -> 7
+ * ------------
+ * 7 -> 0 -> 8
+ * 
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * 
+ * Example 2:
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ * 
+ * Example 3:
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ *
+ * Constraints:
+ * -> The number of nodes in each linked list is in the range [1, 100].
+ * -> 0 <= Node.val <= 9
+ * -> It is guaranteed that the list represents a number that does not have leading zeros.
+ * 
+ */
+public class AddTwoNumbersSolution02
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode head = null, node = null;
+        var decimalPart = 0;
+        while (l1 != null || l2 != null)
+        {
+            var value = decimalPart;
+            if (l1 != null)
+            {
+                value += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null)
+            {
+                value += l2.val;
+                l2 = l2.next;
+            }
+
+            decimalPart = value / 10;
+            var newNode = new ListNode(value % 10, null);
+            if (head == null)
+            {
+                head = node = newNode;
+            }
+            else
+            {
+                node.next = newNode;
+                node = node.next;
+            }
+        }
+
+        if (decimalPart != 0)
+        {
+            node.next = new ListNode(decimalPart, null);
+        }
+
+        return head;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ LeetCodeChallengersTests/LinkedList/AddTwoNumbersSolution02Tests.cs
@@ -0,0 +1,80 @@
+﻿using LeetCodeChallengers.LinkedList;
+using FluentAssertions;
+
+
+namespace LeetCodeChallengersTests.LinkedList;
+
+public class AddTwoNumbersSolution02Tests
+{
+    private AddTwoNumbersSolution02 sut = new AddTwoNumbersSolution02();
+
+    [Fact]
+    public void Test1()
+    {
+        var l1 = CreateList(new List<int> { 2, 4, 3 });
+        var l2 = CreateList(new List<int> { 5, 6, 4 });
+
+        var sum = sut.AddTwoNumbers(l1, l2);
+
+        VerifyResult(sum, new List<int> { 7, 0, 8 });
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var l1 = CreateList(new List<int> { 0 });
+        var l2 = CreateList(new List<int> { 0 });
+
+        var sum = sut.AddTwoNumbers(l1, l2);
+
+        VerifyResult(sum, new List<int> { 0 });
+    }
+
+
+    [Fact]
+    public void Test3()
+    {
+        var l1 = CreateList(new List<int> { 9, 9, 9, 9, 9, 9, 9 });
+        var l2 = CreateList(new List<int> { 9, 9, 9, 9 });
+
+        var sum = sut.AddTwoNumbers(l1, l2);
+
+        VerifyResult(sum, new List<int> { 8, 9, 9, 9, 0, 0, 0, 1 });
+    }
+
+    private void VerifyResult(ListNode listNode, IEnumerable<int> list)
+    {
+        foreach(var num in list)
+        {
+            listNode.Should().NotBeNull();
+            listNode.val.Should().Be(num);
+            listNode = listNode.next;
+        }
+
+        listNode.Should().BeNull();
+    }
+
+    private ListNode CreateList(IEnumerable<int> list)
+    {
+        ListNode last = null;
+        ListNode head = null;
+        foreach(var item in list)
+        {
+            var node = new ListNode
+            {
+                val = item
+            };
+
+            if (last != null)
+            {
+                last.next = node;
+            } else
+            {
+                head = node;
+            }
+            last = node;
+        }
+
+        return head;
+    }
+}
