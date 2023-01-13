a00434c773892ab6c18eefd41b3db68f8dba45f6
Breno Batista Machado <brenomachado@gmail.com>
2023-01-12 21:16:33 -0800

    78 Subsets

---
+++ LeetCodeChallengersTests/RecursionBacktracking/SubsetsSolution78Tests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.RecursionBacktracking;
+
+namespace LeetCodeChallengersTests.RecursionBacktracking;
+
+public class SubsetsSolution78Tests
+{
+    private SubsetsSolution78 sut = new SubsetsSolution78();
+
+    [Fact]
+    public void Test1()
+    {
+        var subsets = sut.Subsets(new int[] { 1, 2, 3 });
+        subsets.Should().BeEquivalentTo(
+            new List<IList<int>>() {
+                new List<int> { },
+                new List<int> { 1 },
+                new List<int> { 2 },
+                new List<int> { 1, 2 },
+                new List<int> { 3 },
+                new List<int> { 1, 3 },
+                new List<int> { 2, 3 },
+                new List<int> { 1, 2, 3 }
+            }
+        );
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var subsets = sut.Subsets(new int[] { 0 });
+        subsets.Should().BeEquivalentTo(
+            new List<IList<int>>() {
+                new List<int> { },
+                new List<int> { 0 }
+            }
+        );
+    }
+}
