06c0cc1cbccc8ff23f907dee72a95fd6d6b6dab2
Breno Batista Machado <brenomachado@gmail.com>
2023-01-12 21:39:26 -0800

    90 Subsets II

---
+++ LeetCodeChallengers/RecursionBacktracking/SubsetsIISolution90.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCodeChallengers.RecursionBacktracking;
+
+/*
+ * 90 Subsets II
+ * 
+ * Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+ * The solution set must not contain duplicate subsets. Return the solution in any order.
+ * 
+ * Example 1:
+ * Input: nums = [1,2,2]
+ * Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+ * 
+ * Example 2:
+ * Input: nums = [0]
+ * Output: [[],[0]]
+ * 
+ * Constraints:
+ * 1 <= nums.length <= 10
+ * -10 <= nums[i] <= 10
+*/
+public class SubsetsIISolution90
+{
+    public IList<IList<int>> SubsetsWithDup(int[] nums)
+    {
+        var result = new List<IList<int>>
+        {
+            new List<int>()
+        };
+        var existingLists = new HashSet<string>();
+
+        foreach (var num in nums)
+        {
+            var newSubsets = new List<IList<int>>();
+            foreach (var existing in result)
+            {
+                var subset = new List<int>();
+                subset.AddRange(existing);
+                subset.Add(num);
+                var subsetString = string.Join(",", subset.OrderBy(n => n));
+                if (!existingLists.Contains(subsetString))
+                {
+                    newSubsets.Add(subset);
+                    existingLists.Add(subsetString);
+                }
+            }
+            result.AddRange(newSubsets);
+        }
+
+        return result.ToList();
+    }
+}
---
+++ LeetCodeChallengersTests/RecursionBacktracking/SubsetsIISolution90Tests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.RecursionBacktracking;
+
+namespace LeetCodeChallengersTests.RecursionBacktracking;
+
+public class SubsetsIISolution90Tests
+{
+    private SubsetsIISolution90 sut = new SubsetsIISolution90();
+
+    [Fact]
+    public void Test1()
+    {
+        var subsets = sut.SubsetsWithDup(new int[] { 1, 2, 2 });
+        subsets.Should().BeEquivalentTo(
+            new List<IList<int>>() {
+                new List<int> { },
+                new List<int> { 1 },
+                new List<int> { 1, 2 },
+                new List<int> { 1, 2, 2 },
+                new List<int> { 2 },
+                new List<int> { 2, 2 }
+            }
+        );
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var subsets = sut.SubsetsWithDup(new int[] { 0 });
+        subsets.Should().BeEquivalentTo(
+            new List<IList<int>>() {
+                new List<int> { },
+                new List<int> { 0 }
+            }
+        );
+    }
+}
