aff0be8453a3f13494868ce7bc111d900422ddce
Breno Batista Machado <brenomachado@gmail.com>
2023-01-15 19:22:14 -0800

    39. Combination Sum

---
+++ LeetCodeChallengers/RecursionBacktracking/CombinationSumSolution39.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCodeChallengers.RecursionBacktracking;
+
+/*
+ * 39. Combination Sum
+ * Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+ * 
+ * The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+ * frequency of at least one of the chosen numbers is different.
+ * The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+ * 
+ * Example 1:
+ * Input: candidates = [2,3,6,7], target = 7
+ * Output: [[2,2,3],[7]]
+ * Explanation:
+ * 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times. 7 is a candidate, and 7 = 7.
+ * These are the only two combinations.
+ * 
+ * Example 2:
+ * Input: candidates = [2,3,5], target = 8
+ * Output: [[2,2,2,2],[2,3,3],[3,5]]
+ * 
+ * Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+ */
+public class CombinationSumSolution39
+{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        Array.Sort(candidates);
+        var result = new List<IList<int>>();
+        var currentList = new List<int>();
+        CombinationSum(candidates, target, result, currentList, 0, 0);
+        return result;
+    }
+
+    private void CombinationSum(int[] candidates, int target, IList<IList<int>> result, IList<int> currentList, int start, int sum)
+    {
+        if (sum > target)
+            return;
+        if (sum == target)
+        {
+            result.Add(new List<int>(currentList));
+            return;
+        }
+        for (int i = start; i < candidates.Length; i++)
+        {
+            currentList.Add(candidates[i]);
+            sum += candidates[i];
+            CombinationSum(candidates, target, result, currentList, i, sum);
+            sum -= candidates[i];
+            currentList.RemoveAt(currentList.Count - 1);
+        }
+    }
+}
---
+++ LeetCodeChallengersTests/RecursionBacktracking/CombinationSumSolution39Tests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.RecursionBacktracking;
+
+namespace LeetCodeChallengersTests.RecursionBacktracking;
+
+public class CombinationSumSolution39Tests
+{
+    private CombinationSumSolution39 sut = new CombinationSumSolution39();
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.CombinationSum(new int[] { 2, 3, 7, 9 }, 7);
+        result.Should().BeEquivalentTo(new List<IList<int>>
+        {
+            new List<int> { 2, 2, 3 },
+            new List<int> { 7 }
+        });
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.CombinationSum(new int[] { 2, 3, 5 }, 8);
+        result.Should().BeEquivalentTo(new List<IList<int>>
+        {
+            new List<int> { 2, 2, 2, 2 },
+            new List<int> { 2, 3, 3 },
+            new List<int> { 3, 5 }
+        });
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = sut.CombinationSum(new int[] { 2 }, 1);
+        result.Should().BeEquivalentTo(new List<IList<int>>());
+    }
+}
