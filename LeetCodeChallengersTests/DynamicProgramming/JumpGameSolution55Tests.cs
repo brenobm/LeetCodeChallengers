0b694514ef139cd0cad5037b8d401ea331b4c6c5
Breno Batista Machado <brenomachado@gmail.com>
2023-01-17 22:53:54 -0800

    55. Jump Game

---
+++ LeetCodeChallengersTests/DynamicProgramming/JumpGameSolution55Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.DynamicProgramming;
+
+namespace LeetCodeChallengersTests.DynamicProgramming;
+
+public class JumpGameSolution55Tests
+{
+    private JumpGameSolution55 sut = new JumpGameSolution55();
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.CanJump(new int[] { 2, 3, 1, 1, 4 });
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.CanJump(new int[] { 3, 2, 1, 0, 4 });
+        result.Should().BeFalse();
+    }
+}
