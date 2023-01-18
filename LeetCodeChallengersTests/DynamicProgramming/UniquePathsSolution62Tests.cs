cd56fc3f6ce476ffe30063eb7704c69a5c8b4694
Breno Batista Machado <brenomachado@gmail.com>
2023-01-18 00:15:24 -0800

    62. Unique Paths

---
+++ LeetCodeChallengersTests/DynamicProgramming/UniquePathsSolution62Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.DynamicProgramming;
+
+namespace LeetCodeChallengersTests.DynamicProgramming;
+
+public class UniquePathsSolution62Tests
+{
+    private UniquePathsSolution62 sut = new UniquePathsSolution62();
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.UniquePaths(3, 7);
+        result.Should().Be(28);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.UniquePaths(3, 2);
+        result.Should().Be(3);
+    }
+}
