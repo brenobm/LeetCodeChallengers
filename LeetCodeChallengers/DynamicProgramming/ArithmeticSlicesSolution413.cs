dcc695e138eabf0df0b516f28f16810f81b2ff54
Breno Batista Machado <brenomachado@gmail.com>
2023-01-19 22:17:17 -0800

    413. Arithmetic Slices

---
+++ LeetCodeChallengers/DynamicProgramming/ArithmeticSlicesSolution413.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCodeChallengers.DynamicProgramming;
+
+/*
+ * 413. Arithmetic Slices
+ * 
+ * An integer array is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+ * For example, [1,3,5,7,9], [7,7,7,7], and [3,-1,-5,-9] are arithmetic sequences.
+ * Given an integer array nums, return the number of arithmetic subarrays of nums.
+ * A subarray is a contiguous subsequence of the array.
+ * 
+ * Example 1:
+ * Input: nums = [1,2,3,4]
+ * Output: 3
+ * Explanation: We have 3 arithmetic slices in nums: [1, 2, 3], [2, 3, 4] and [1,2,3,4] itself.
+ * 
+ * Example 2:
+ * Input: nums = [1]
+ * Output: 0
+ * 
+ * Constraints:
+ * 1 <= nums.length <= 5000
+ * -1000 <= nums[i] <= 1000
+ */
+public class ArithmeticSlicesSolution413
+{
+    public int NumberOfArithmeticSlices(int[] nums)
+    {
+        var numSubArrays = 0;
+
+        var diff = 0;
+        var count = 1;
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (diff == nums[i + 1] - nums[i] && i > 0)
+            {
+                count++;
+            }
+            else
+            {
+                numSubArrays += CountSubArrays(count);
+                count = 1;
+                diff = nums[i + 1] - nums[i];
+            }
+        }
+
+        numSubArrays += CountSubArrays(count);
+        return numSubArrays;
+    }
+
+    private int CountSubArrays(int count)
+    {
+        var numSubArrays = 0;
+        if (count >= 2)
+        {
+            for (int j = count - 1; j > 0; j--)
+            {
+                numSubArrays += j;
+            }
+        }
+        return numSubArrays;
+    }
+}
---
+++ LeetCodeChallengersTests/DynamicProgramming/ArithmeticSlicesSolution413Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.DynamicProgramming;
+
+namespace LeetCodeChallengersTests.DynamicProgramming;
+
+public class ArithmeticSlicesSolution413Tests
+{
+    private ArithmeticSlicesSolution413 sut = new ArithmeticSlicesSolution413();
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.NumberOfArithmeticSlices(new int[] { 1, 2, 3, 4});
+        result.Should().Be(3);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.NumberOfArithmeticSlices(new int[] { 1 });
+        result.Should().Be(0);
+    }
+}
