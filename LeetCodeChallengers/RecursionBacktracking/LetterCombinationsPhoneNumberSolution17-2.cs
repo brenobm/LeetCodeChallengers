78d1c5c2c5a7fc5118bb1e621a6375e6fde67df2
Breno Batista Machado <brenomachado@gmail.com>
2024-01-08 16:55:50 -0300

    17. Letter Combinations of a Phone Number - Solution 2 improved removing recursion and including loop. Migrated to .Net 8

--- LeetCodeChallengers/RecursionBacktracking/LetterCombinationsPhoneNumberSolution17-2.cs
+++ LeetCodeChallengers/RecursionBacktracking/LetterCombinationsPhoneNumberSolution17-2.cs
@@ -37,59 +37,59 @@ namespace LeetCodeChallengers.RecursionBacktracking;
  */
 public class LetterCombinationsPhoneNumberSolution17_2
 {
-    private static readonly Dictionary<char, List<char>> _digitsMap = new()
+    private static readonly Dictionary<char, char[]> _digitsMap = new()
     {
-        { '1', new() },
-        { '2', new(){ 'a', 'b', 'c' } },
-        { '3', new(){ 'd', 'e', 'f' } },
-        { '4', new(){ 'g', 'h', 'i' } },
-        { '5', new(){ 'j', 'k', 'l' } },
-        { '6', new(){ 'm', 'n', 'o' } },
-        { '7', new(){ 'p', 'q', 'r', 's' } },
-        { '8', new(){ 't', 'u', 'v' } },
-        { '9', new(){ 'w', 'x', 'y', 'z' } },
+        { '1', [] },
+        { '2', [ 'a', 'b', 'c' ] },
+        { '3', [ 'd', 'e', 'f' ] },
+        { '4', [ 'g', 'h', 'i' ] },
+        { '5', [ 'j', 'k', 'l' ] },
+        { '6', [ 'm', 'n', 'o' ] },
+        { '7', [ 'p', 'q', 'r', 's' ] },
+        { '8', [ 't', 'u', 'v' ] },
+        { '9', [ 'w', 'x', 'y', 'z' ] },
     };
 
     public IList<string> LetterCombinations(string digits)
     {
-        return LetterCombinations(digits, 0, new List<StringBuilder>()).Select(sb => sb.ToString()).ToList();
-    }
-
-    private IList<StringBuilder> LetterCombinations(string digits, int digitIndex, IList<StringBuilder> oldResult)
-    {
-        if (digitIndex >= digits.Length)
-        {
-            return oldResult;
-        }
-
-        var digit = digits[digitIndex];
-
-        var letters = _digitsMap[digit];
-
-        if (oldResult.Count == 0 && letters.Count > 0)
-        {
-            oldResult.Add(new StringBuilder());
-        }
-
         var result = new List<StringBuilder>();
 
-        while (oldResult.Any())
+        for (int digitIndex = 0; digitIndex < digits.Length; digitIndex++)
         {
-            var item = oldResult[0];
-            oldResult.RemoveAt(0);
-            for (var index = 0; index < letters.Count; index++)
+            var letters = _digitsMap[digits[digitIndex]];
+
+            if (!letters.Any())
             {
-                if (index == letters.Count - 1)
+                continue;
+            }
+
+            if (!result.Any())
+            {
+                result.Add(new StringBuilder());
+            }
+
+            var tempList = new List<StringBuilder>();
+
+            while (result.Any())
+            {
+                var item = result[0];
+                result.RemoveAt(0);
+                for (var index = 0; index < letters.Length; index++)
                 {
-                    result.Add(item.Append(letters[index]));
-                }
-                else
-                {
-                    result.Add(new StringBuilder().Append(item).Append(letters[index]));
+                    if (index == letters.Length - 1)
+                    {
+                        tempList.Add(item.Append(letters[index]));
+                    }
+                    else
+                    {
+                        tempList.Add(new StringBuilder().Append(item).Append(letters[index]));
+                    }
                 }
             }
+
+            result = tempList;
         }
 
-        return LetterCombinations(digits, digitIndex + 1, result);
+        return result.Select(sb => sb.ToString()).ToList();
     }
 }
