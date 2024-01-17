842b1dbec201abd7011b5027936aef27c9f4fe28
Breno Batista Machado <brenomachado@gmail.com>
2024-01-17 20:13:10 -0300

    392. Is Subsequence

---
+++ LeetCodeChallengersTests/TwoPointer/IsSubsequenceSolution392Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.TwoPointer;
+
+namespace LeetCodeChallengersTests.TwoPointer;
+
+public class IsSubsequenceSolution392Tests
+{
+    private readonly IsSubsequenceSolution392 sut = new IsSubsequenceSolution392();
+
+    [Fact]
+    public void Test1()
+    {
+        var result = sut.IsSubsequence("abc", "ahbgdc");
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = sut.IsSubsequence("axc", "ahbgdc");
+        result.Should().BeFalse();
+    }
+}
