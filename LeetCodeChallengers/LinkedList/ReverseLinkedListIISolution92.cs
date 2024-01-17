32838108890bb7edca329b9c0ba5200640468e93
Breno Batista Machado <brenomachado@gmail.com>
2024-01-16 22:35:37 -0300

    92. Reverse Linked List II

---
+++ LeetCodeChallengers/LinkedList/ReverseLinkedListIISolution92.cs
@@ -0,0 +1,93 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace LeetCodeChallengers.LinkedList;
+
+/*
+ * 92. Reverse Linked List II
+ * 
+ * Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+ * 
+ * Example 1:
+ * Input: head = [1,2,3,4,5], left = 2, right = 4
+ * Output: [1,4,3,2,5]
+ * 
+ * Example 2:
+ * Input: head = [5], left = 1, right = 1
+ * Output: [5]
+ * 
+ * Constraints:
+ * The number of nodes in the list is n.
+ * 1 <= n <= 500
+ * -500 <= Node.val <= 500
+ * 1 <= left <= right <= n
+ * 
+ * Follow up: Could you do it in one pass?
+ */
+public class ReverseLinkedListIISolution92
+{
+    public ListNode ReverseBetween(ListNode head, int left, int right)
+    {
+        if (left == right)
+        {
+            return head;
+        }
+
+        var tail = null as ListNode;
+        var start = head;
+
+        for (var i = 0; i < left - 1; i++)
+        {
+            tail = start;
+            start = start.next;
+        }
+
+        var cur = start;
+        var tmp = null as ListNode;
+        var old = null as ListNode;
+
+        for (var i = left; i < right + 1; i++)
+        {
+            tmp = cur.next;
+            cur.next = old;
+            old = cur;
+            cur = tmp;
+        }
+
+        if (tail != null)
+        {
+            tail.next = old;
+        }
+        else
+        {
+            head = old;
+        }
+
+        start.next = cur;
+
+        return head;
+    }
+}
+
+//          <-
+//1  ->  2  ->  3      4  ->  5
+//              c      
+//       s
+//       l 
+//                     t
+//              p
+
+//i - 3
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+
---
+++ LeetCodeChallengersTests/LinkedList/ReverseLinkedListIISolution92Tests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.LinkedList;
+
+namespace LeetCodeChallengersTests.LinkedList;
+
+public class ReverseLinkedListIISolution92Tests
+{
+    private readonly ReverseLinkedListIISolution92 sut = new ReverseLinkedListIISolution92();
+
+    [Fact]
+    public void Test1()
+    {
+        var list = new ListNode
+        {
+            val = 1,
+            next = new()
+            {
+                val = 2,
+                next = new()
+                {
+                    val = 3,
+                    next = new()
+                    {
+                        val = 4,
+                        next = new()
+                        {
+                            val = 5
+                        }
+                    }
+                }
+            }
+        };
+
+        var result = sut.ReverseBetween(list, 2, 4);
+
+        ValidateLinkedList(result, [1, 4, 3, 2, 5]);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var list = new ListNode
+        {
+            val = 5
+        };
+
+        var result = sut.ReverseBetween(list, 1, 1);
+
+        ValidateLinkedList(result, [5]);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var list = new ListNode
+        {
+            val = 3,
+            next = new()
+            {
+                val = 5,
+            },
+        };
+
+        var result = sut.ReverseBetween(list, 1, 2);
+
+        ValidateLinkedList(result, [5, 3]);
+    }
+
+    private static void ValidateLinkedList(ListNode head, int[] expected)
+    {
+        if (expected.Length == 0)
+        {
+            head.Should().BeNull();
+        }
+
+        for (int i = 0; i < expected.Length; i++)
+        {
+            head.Should().NotBeNull();
+            head.val.Should().Be(expected[i]);
+
+            if (i == expected.Length - 1)
+            {
+                head.next.Should().BeNull();
+            }
+
+            head = head.next;
+        }
+    }
+}
