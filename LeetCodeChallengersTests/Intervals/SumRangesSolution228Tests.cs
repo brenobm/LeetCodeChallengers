0df3f90599427c9e0db573cb51138354a0c9a727
Breno Batista Machado <brenomachado@gmail.com>
2024-01-18 15:09:48 -0300

    228. Sum Ranges

---
+++ LeetCodeChallengersTests/Intervals/SumRangesSolution228Tests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.Intervals;
+
+namespace LeetCodeChallengersTests.Intervals;
+
+public class SumRangesSolution228Tests
+{
+    private readonly SumRangesSolution228 sut = new SumRangesSolution228();
+
+    public static IEnumerable<object[]> GetTestData =>
+        new List<object[]>
+        {
+            new object[] { new int[] { 0, 1, 2, 4, 5, 7 }, new List<string> { "0->2","4->5","7" } },
+            new object[] { new int[] { 0, 2, 3, 4, 6, 8, 9 }, new List<string> { "0", "2->4", "6", "8->9" } },
+            new object[] { new int[] { }, new List<string>() },
+        };
+
+
+    [Theory, MemberData(nameof(GetTestData))]
+    public void Test(int[] input, IList<string> output)
+    {
+        var result = sut.SummaryRanges(input);
+        result.Should().BeEquivalentTo(output);
+    }
+}
