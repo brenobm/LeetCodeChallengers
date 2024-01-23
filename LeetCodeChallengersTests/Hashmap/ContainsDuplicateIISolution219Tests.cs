a260181b39e8a8d31ef2d6c53c7ce343bc4cdfbb
Breno Batista Machado <brenomachado@gmail.com>
2024-01-23 15:29:43 -0300

    219. Contains Duplicate II

---
+++ LeetCodeChallengersTests/Hashmap/ContainsDuplicateIISolution219Tests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.Hashmap;
+
+namespace LeetCodeChallengersTests.Hashmap;
+
+public class ContainsDuplicateIISolution219Tests
+{
+    private readonly ContainsDuplicateIISolution219 sut = new ContainsDuplicateIISolution219();
+
+    public static IEnumerable<object[]> GetTestData =>
+        new List<object[]>
+        {
+                new object[] { new int[] { 1, 2, 3, 1 }, 3, true },
+                new object[] { new int[] { 1, 0, 1, 1 }, 1, true },
+                new object[] { new int[] { 1, 2, 3, 1, 2, 3 }, 2, false },
+        };
+
+
+    [Theory, MemberData(nameof(GetTestData))]
+    public void Test(int[] input1, int input2, bool output)
+    {
+        var result = sut.ContainsNearbyDuplicate(input1, input2);
+        result.Should().Be(output);
+    }
+
+}
