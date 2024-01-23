fdeeee3f39daecd16370d733c008a1ae135b099b
Breno Batista Machado <brenomachado@gmail.com>
2024-01-23 15:14:36 -0300

    383. Ransom Note

---
+++ LeetCodeChallengers/Hashmap/RansomNoteSolution383.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCodeChallengers.Hashmap;
+
+/*
+ * 383. Ransom Note
+ * 
+ * Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+ * Each letter in magazine can only be used once in ransomNote.
+ * 
+ * Example 1:
+ * Input: ransomNote = "a", magazine = "b"
+ * Output: false
+ * 
+ * Example 2:
+ * Input: ransomNote = "aa", magazine = "ab"
+ * Output: false
+ * 
+ * Example 3:
+ * Input: ransomNote = "aa", magazine = "aab"
+ * Output: true
+ * 
+ * Constraints:
+ * 1 <= ransomNote.length, magazine.length <= 105
+ * ransomNote and magazine consist of lowercase English letters.
+ */
+public class RansomNoteSolution383
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var magazineLetters = new Dictionary<char, int>();
+        
+        foreach (var letter in magazine)
+        {
+            if (!magazineLetters.ContainsKey(letter))
+            {
+                magazineLetters.Add(letter, 1);
+            }
+            else
+            {
+                magazineLetters[letter]++;
+            }
+        }
+
+        foreach (var letter in ransomNote)
+        {
+            if (!magazineLetters.ContainsKey(letter) || magazineLetters[letter] == 0)
+            {
+                return false;
+            }
+
+            magazineLetters[letter]--;
+        }
+
+        return true;
+    }
+}
---
+++ LeetCodeChallengersTests/Hashmap/RansomNoteSolution383Tests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.Hashmap;
+
+namespace LeetCodeChallengersTests.Hashmap;
+
+public class RansomNoteSolution383Tests
+{
+    private readonly RansomNoteSolution383 sut = new RansomNoteSolution383();
+
+    public static IEnumerable<object[]> GetTestData =>
+        new List<object[]>
+        {
+                new object[] { "a", "b", false },
+                new object[] { "aa", "ab", false },
+                new object[] { "aa", "aab", true },
+        };
+
+
+    [Theory, MemberData(nameof(GetTestData))]
+    public void Test(string input1, string input2, bool output)
+    {
+        var result = sut.CanConstruct(input1, input2);
+        result.Should().Be(output);
+    }
+}
