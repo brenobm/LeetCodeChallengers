3abfc7a68e14599325c3934a5a85dde0390dd300
Breno Batista Machado <brenomachado@gmail.com>
2024-10-28 16:52:59 -0700

    Climbing Stairs - Solution 70

---
+++ LeetCodeChallengersTests/DynamicProgramming/ClimbingStairsSolution70Tests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using LeetCodeChallengers.DynamicProgramming;
+
+namespace LeetCodeChallengersTests.DynamicProgramming;
+
+public class ClimbingStairsSolution70Tests
+{
+    private readonly ClimbingStairsSolution70 sut = new ClimbingStairsSolution70();
+
+    public static IEnumerable<object[]> GetTestData =>
+        new List<object[]>
+        {
+                new object[] { 2, 2 },
+                new object[] { 3, 3 },
+        };
+
+
+    [Theory, MemberData(nameof(GetTestData))]
+    public void Test(int input, int output)
+    {
+        var result = sut.ClimbStairs(input);
+        result.Should().BeEquivalentTo(output);
+    }
+}
